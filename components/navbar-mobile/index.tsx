56c450b35c38a24aff7028579f453a178d195df4
luiz-divino <nandodivinodev@gmail.com>
2026-08-14 19:51:23 -0300

    feat: implement useState to control opening of the menu

--- components/navbar-mobile/index.tsx
+++ components/navbar-mobile/index.tsx
@@ -1,38 +1,41 @@
-'use client'
-import { TextAlignJustify } from "lucide-react"
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet"
-import { navlinks } from "@/lib/services"
-import Link from "next/link"
-
+"use client";
+import { TextAlignJustify } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import { navlinks } from "@/lib/services";
+import Link from "next/link";
+import { useState } from "react";
 
 export const Navbar = () => {
-    return (
-        <Sheet open >
-            <SheetTrigger>
-                <TextAlignJustify/>
-            </SheetTrigger>
+  const [open, setOpen] = useState(false);
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger>
+        <TextAlignJustify />
+      </SheetTrigger>
 
-            <SheetContent>
-                <SheetHeader>
-                    <div>
-                        <SheetTitle>
-                            Titulo
-                        </SheetTitle>
-                    </div>
-                </SheetHeader>
+      <SheetContent>
+        <SheetHeader>
+          <div>
+            <SheetTitle>Titulo</SheetTitle>
+          </div>
+        </SheetHeader>
 
-                <div>
-                    {
-                        navlinks.map((item)=> (
-                            <ul key={item.label}>
-                                <li>
-                                    <Link href={item.href}>{item.label}</Link>
-                                </li>
-                            </ul>
-                        ))
-                    }
-                </div>
-            </SheetContent>
-        </Sheet>
-    )
-}
+        <div>
+          {navlinks.map((item) => (
+            <ul key={item.label}>
+              <li>
+                <Link href={item.href}>{item.label}</Link>
+              </li>
+            </ul>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
